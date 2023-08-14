920d539a69d437f3638985baaafc397034b3ebfd
qorahoja <132049921+qorahoja@users.noreply.github.com>
2023-08-14 12:22:10 +0500

    Add files via upload

--- For_school/bot.py
+++ For_school/bot.py
@@ -2,7 +2,6 @@ import logging
 import csv
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
 
 # Bot toki (API token)ni quyidagi o'zgaruvchiga o'zgartiring
 BOT_TOKEN = '6013857528:AAGH9iV9PI8WEg2-TCkNpxLOtWqSDQHgCmM'
@@ -14,33 +13,49 @@ logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
 
 
+# Foydalanuvchi ma'lumotlarini saqlash uchun raqam holati
+class Form:
+    waiting_for_name = 1
+    waiting_for_phone = 2
+
+
+user_data = {}
+
+
+
+
+
 # /start buyrug'iga javob berish
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(text="Kontakt ma'lumotlarini jo'nating", request_contact=True)
-    keyboard.add(button)
+    await message.reply("Assalomu alaykom! Men 129-maktab uchun yozilgan botman. Iltimos farzandingizni ismini kiriting: ")
 
-    await message.reply("Assalomu alaykom men 129-maktab uchun yozilgan botman iltimos bolangizni maktabga kelish ketishini bilmoqchi bo'lsangiz menga kontaktingizni yuboring:",
-                        reply_markup=keyboard)
-    await message.reply(
-        "Sizga har kuni farzndingiz maktabga soat nechida va qachon kirayotgani va chiqayotganini aniq yetkazamiz:")
+    user_data[message.from_user.id] = {"state": Form.waiting_for_name}
 
 
+# Foydalanuvchi xabarlari bilan ishlash
+@dp.message_handler(lambda message: user_data.get(message.from_user.id) and user_data[message.from_user.id]["state"] == Form.waiting_for_name)
+async def process_name(message: types.Message):
+    user_data[message.from_user.id]["name"] = message.text
+    user_data[message.from_user.id]["state"] = Form.waiting_for_phone
 
-# Foydalanuvchi kontaktni jo'natganda ishlaydigan funksiya
-@dp.message_handler(content_types=types.ContentType.CONTACT)
-async def process_contact(message: types.Message):
-    contact = message.contact
-    contact_info = f"Kontakt nomi: {contact.first_name}\nTelefon raqami: {contact.phone_number}"
-    await message.reply( "Bizga ishonch bildirganingiz uchun rahmat")
+    await message.reply(f"Ismingiz: {message.text}\nIltimos, telefon raqamingizni kiriting(misol uchun 9989********):")
 
-    await message.reply(contact_info, parse_mode=ParseMode.MARKDOWN)
 
-    # Kontakt ma'lumotlarini CSV faylga saqlash
+@dp.message_handler(lambda message: user_data.get(message.from_user.id) and user_data[message.from_user.id]["state"] == Form.waiting_for_phone)
+async def process_phone(message: types.Message):
+    user_id = message.from_user.id
+    user_name = user_data[user_id]["name"]
+    user_phone = message.text
+
+    await message.reply("Bergan ma'lumotlaringiz uchun rahmat farzandingiz <strong>davomati</strong> haqida ma'lumot u maktabga har kirganida va chiqganida sizga berib boriladi",parse_mode="html")
+
     with open('users_data.csv', 'a', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow([contact.phone_number, contact.first_name])
+        csv_writer.writerow([user_name, user_phone, user_id])
+
+    user_data[user_id]["state"] = None
+
 
 
 if __name__ == '__main__':
